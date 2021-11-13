e544e610ad03571d96a5b32c5fd15a21c62b7dd9
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-13 14:09:08 +0800

    next js 全局Head

--- nextjs-blog/pages/_app.js
+++ nextjs-blog/pages/_app.js
@@ -1,7 +1,16 @@
 import '../styles/globals.css'
+import Head from 'next/head'
 
-function MyApp({ Component, pageProps }) {
-  return <Component {...pageProps} />
+function App({ Component, pageProps }) {
+  return (
+    <>
+      <Head>
+        <title>我的博客</title>
+        <meta name="viewport" content="width=device-width, initial-scale=1.0" />
+      </Head>
+      <Component {...pageProps} />
+    </>
+  )
 }
 
-export default MyApp
+export default App
--- nextjs-blog/pages/posts/first-posts.js
+++ nextjs-blog/pages/posts/first-posts.js
@@ -1,7 +1,14 @@
 import React from 'react'
+import Link from 'next/link'
 
-export default function X() {
+export default function FirstPost() {
     return (
-        <div>First Post</div>
+        <>
+            <div>
+                First Post
+                <hr />
+                <Link href="/"><a >回到首页</a></Link>
+            </div>
+        </>
     )
 }
