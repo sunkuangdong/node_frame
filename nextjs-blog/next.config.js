6d983f0e6a4f66180154ef74ba7523ca87e4e744
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-13 19:28:02 +0800

    完成next入门

--- nextjs-blog/next.config.js
+++ nextjs-blog/next.config.js
@@ -1,3 +1,13 @@
 module.exports = {
   reactStrictMode: true,
 }
+
+/*
+  next-images 配置无效
+*/
+// const withImages = require('next-images')
+// module.exports = withImages({
+//   webpack(config, options) {
+//     return config
+//   }
+// })
