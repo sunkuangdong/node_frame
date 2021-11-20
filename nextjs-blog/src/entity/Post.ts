38c128465c50e1b9ccfd5ba90d0bb3ad49009b19
sunsunkuangdong <sunkuangdong@edianzu.com>
2021-11-20 10:58:22 +0800

    生成表

--- nextjs-blog/src/entity/Post.ts
+++ nextjs-blog/src/entity/Post.ts
@@ -4,7 +4,10 @@ import { Column, Entity, PrimaryGeneratedColumn } from "typeorm";
 export class Post {
     // PrimaryGeneratedColumn 主键 increment 自增长
     @PrimaryGeneratedColumn("increment") id: number;
-    // id 对应的是 int Column 里面对应的是数据库的类型
-    @Column("varchar") title: string;
-    @Column("text") content: string;
+    // title 对应的是 string Column 里面对应的是数据库的类型
+    @Column("varchar") title: string
+    @Column("text") content: string
+    constructor(attributes: Partial<Post>) {
+        Object.assign(this, attributes);
+    }
 }
